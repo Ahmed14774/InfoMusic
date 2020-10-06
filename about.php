ab5b4bc632fed640d765948cce942c664c805ca4
Ahmed Ahmed <59990581+Ahmed14774@users.noreply.github.com>
2020-10-06 16:32:19 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,29 @@
+<!-- Include Header -->
+<?php
+  require "header.php";
+?>
+<link rel="stylesheet" href="Styles/about.css">
+<!-- Page Content -->
+<div class="main">
+  <div class="about-us">
+    <h2 class="about-title">We Do It For The Community</h2>
+    <p class="about-title1">People love music and there is always a need for sites and applications that can provide people with the music they are looking for.</p>
+  </div>
+  <div class="about-text">
+    <h2 class="title">Vision</h2>
+    <p class="title-text2">
+      At InfoMusic, we believe there is a better way for our customers and artists to express themselves than other convention streaming services.
+      Our music website makes it easy to search for the artists and genres you like to listen to.
+      You get to create your own account, purchase music and add it to your playlist.
+      This website will also be a great opportunity for artists that want their music to be heard by others.
+      The artists can connect their social media accounts to our website and introduce their music by uploading to our website.
+    </p>
+  </div>
+</div>
+
+
+<!-- End Page Content -->
+<!-- Includ Footer -->
+<?php
+  require "footer.php";
+?>

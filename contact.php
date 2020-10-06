35fbd21e1f55071f350f784a6b8ef225b33c23e3
Ahmed Ahmed <59990581+Ahmed14774@users.noreply.github.com>
2020-10-06 16:32:46 -0500

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!-- Include Header -->
+
+<?php
+  require "header.php";
+?>
+<!-- Page Content -->
+
+  <div class="">
+
+  </div>
+
+<!-- End Page Content -->
+
+<!-- Includ Footer -->
+
+<?php
+  require "footer.php";
+?>

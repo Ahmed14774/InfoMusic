e79241d22461d3a4d9d005fae0d336cea4ae1547
Ahmed Ahmed <59990581+Ahmed14774@users.noreply.github.com>
2020-10-06 16:33:12 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+    <footer id="footer">
+
+      <div class="socials">
+        <a href="https://twitter.com"><i class="fab ic fa-twitter"></i></a>
+        <a href="https://facebook.com"><i class="fab ic fa-facebook-f"></i></a>
+        <a href="https://instagram.com"><i class="fab ic fa-instagram"></i></a>
+        <a href="https://gmail.com"><i class="fas ic fa-envelope"></i></a>
+        <br>
+        <p class="copyright">© Copyright 2020 InfoMusic</p>
+      </div>
+    </footer>
+  </body>
+</html>
